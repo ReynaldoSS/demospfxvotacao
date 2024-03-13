0dbaad3a422d3b1ea96071b6b89e0e812109096d
Reynaldo Santos <reynaldo.ss@gmail.com>
2024-03-13 19:30:03 -0300

    WebPart Votação de Projetos iniciada

---
+++ src/webparts/votacaoProjetos/components/IVotacaoProjetosProps.ts
@@ -0,0 +1,7 @@
+export interface IVotacaoProjetosProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/votacaoProjetos/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IVotacaoProjetosWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'VotacaoProjetosWebPartStrings' {
+  const strings: IVotacaoProjetosWebPartStrings;
+  export = strings;
+}
