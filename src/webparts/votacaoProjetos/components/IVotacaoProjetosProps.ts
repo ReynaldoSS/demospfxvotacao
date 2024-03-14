1dcdd9f7097dc68eb9057da1be02d0b52e9498a3
Reynaldo Santos <reynaldo.ss@gmail.com>
2024-03-13 23:03:26 -0300

    React com Hooks Funcionando

--- src/webparts/votacaoProjetos/components/IVotacaoProjetosProps.ts
+++ src/webparts/votacaoProjetos/components/IVotacaoProjetosProps.ts
@@ -1,7 +1,10 @@
+import { SPHttpClient } from "@microsoft/sp-http";
 export interface IVotacaoProjetosProps {
   description: string;
   isDarkTheme: boolean;
   environmentMessage: string;
   hasTeamsContext: boolean;
   userDisplayName: string;
+  currentSiteUrl:string;
+  spHttpClient:SPHttpClient;
 }
--- src/webparts/votacaoProjetos/components/VotacaoProjetos.tsx
+++ src/webparts/votacaoProjetos/components/VotacaoProjetos.tsx
@@ -1,43 +1,70 @@
-import * as React from 'react';
-import styles from './VotacaoProjetos.module.scss';
-import type { IVotacaoProjetosProps } from './IVotacaoProjetosProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import styles from "./VotacaoProjetos.module.scss";
+import type { IVotacaoProjetosProps } from "./IVotacaoProjetosProps";
+import { escape } from "@microsoft/sp-lodash-subset";
+import { SPHttpClient, SPHttpClientResponse } from "@microsoft/sp-http";
 
-const VotacaoProjetos : React.FC<IVotacaoProjetosProps> = (props)=> {
-  
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = props;
+const VotacaoProjetos: React.FC<IVotacaoProjetosProps> = (props) => {
+  const {
+    isDarkTheme,
+    hasTeamsContext,
+    userDisplayName,
+    currentSiteUrl,
+    spHttpClient,
+  } = props;
 
-    return (
-      <section className={`${styles.votacaoProjetos} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
+  const [counter, setCounter] = React.useState<number>(1);
+  const [evenOdd, setEvenOdd] = React.useState<string>("");
+  const [siteLists, setSiteLists] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      const endpoint: string = `${currentSiteUrl}/_api/web/lists?$select=Title&$filter=Hidden eq false&$orderby=Title&$top=10`;
+      const rawResponse: SPHttpClientResponse = await spHttpClient.get(
+        endpoint,
+        SPHttpClient.configurations.v1
+      );
+      setSiteLists(
+        (await rawResponse.json()).value.map((list: { Title: string }) => {
+          return list.Title;
+        })
+      );
+    })();
+  },[]);
+
+  const onButtonClick = (): void => {
+    setCounter(counter + 1);
+  };
+
+  React.useEffect(() => {
+    setEvenOdd(counter % 2 === 0 ? "even" : "odd");
+  }, [counter]);
+  return (
+    <section
+      className={`${styles.votacaoProjetos} ${
+        hasTeamsContext ? styles.teams : ""
+      }`}
+    >
+      <div className={styles.welcome}>
+        <img
+          alt=""
+          src={
+            isDarkTheme
+              ? require("../assets/welcome-dark.png")
+              : require("../assets/welcome-light.png")
+          }
+          className={styles.welcomeImage}
+        />
+        <h2>Well done, {escape(userDisplayName)}!</h2>
         <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
+          Counter: <strong>{counter}</strong> is <strong>{evenOdd}</strong>
         </div>
-      </section>
-    );
-  }
- export default VotacaoProjetos;
+        <button onClick={() => onButtonClick()}>+</button>
+        <ul>
+          {siteLists.map((list:string)=>(<li>{list}</li>))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+export default VotacaoProjetos;
